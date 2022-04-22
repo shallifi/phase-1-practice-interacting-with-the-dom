39186e1b94e9d36e607f02973fa978e5b61839e7
Benjamin <shallifi@hotmail.com>
2022-04-21 22:50:19 -0400

    working toggle

--- js/challenge.js
+++ js/challenge.js
@@ -1,31 +1,68 @@
 //timer #counter should increment each second
-const bigCounter = document.querySelector("#counter");
-console.log(let 'numCounter' = parseInt(bigCounter.innerText);
+let bigCounter = document.querySelector("#counter");
 
 //counter interval
-// A Counter that increases by 1 each second
-// let timer = setInterval(fnLog, 1000,bigCounter);
+let intervalID = setInterval(function () {
+    return bigCounter.innerText++;
+}, 1000);
 
-function timer() {
-    parseInt(bigCounter.innerText);
-    // setInterval(timer, 1000, bigCounter.innerText)
-
-  console.log("Function fnLog executed");
+function startTimer() {
+  intervalID = setInterval(function () {
+    return bigCounter.innerText++;
+}, 1000);
 }
-timer()
 
 // Plus and Minus buttons that increment or decrement the counter
-// buttons should change the counter innerText by one
-
 const minusBtn = document.querySelector("#minus");
 const plusBtn = document.querySelector("#plus");
 const heartBtn = document.querySelector("#heart");
 const pauseBtn = document.querySelector("#pause");
 
 minusBtn.addEventListener("click",() => {
-    bigCounter--;
-    console.log(minusBtn);
+      return bigCounter.innerText--;
 })
+
+plusBtn.addEventListener("click", () => {
+    return bigCounter.innerText++;
+})
+//changing the button text 
+const btn = document.getElementById('btn');
+
+//timer pause function needs to  VALUE clearinterval, resume VALUE 
+function stopTimer() {
+  return clearInterval(intervalID);  
+}
+
+function resumeButton() {
+  if (pauseBtn.innerText = "resume") {    
+      resume (bigCounter.innerText, 1000);
+  }
+  resumeButton()
+}
+// //////// Toggle button text on click
+///this one works for eventListener
+////
+
+const toggleText = ("click", function() {
+  if(pauseBtn.innerText === "pause") {
+    pauseBtn.innerText = "resume";
+    stopTimer()
+  }
+  else{
+    pauseBtn.innerText = "pause";
+    startTimer()
+  }
+}); 
+
+pauseBtn.addEventListener("click", toggleText)
+// stopTimer)
+
+// console.log(pauseBtn.innerText)
+//   intervalID(function () {
+//       return bigCounter.innerText++;
+//   }, 1000);
+//   below only adds one number when it is pressed
+//   return bigCounter.innerText++,1000;
 
 
 
@@ -39,6 +76,33 @@ minusBtn.addEventListener("click",() => {
 // A comment box that adds comments when submitted
 
 
-// setInterval(function(){
-//     var a=document.getElementById("counter"),
-// b=parseInt(a.innerText);a.innerText=b+1},1e3)},interval=timer()
+
+// pauseBtn.addEventListener('click', stopTimer) 
+
+// function  handleClick(e) {
+  
+  //   if (pauseBtn.textContent = "pause" {
+    //     return pauseBtn.textContent = 'resume'
+    //     }
+    
+    //   } else (pauseBtn.textContent = 'resume') {
+      //     return pauseBtn.textContent = 'pause'
+      //   };
+      // };
+      // console.log(pauseBtn.innerText);
+      ////////////////
+      // function togglePause() {
+        
+      //   if (pauseBtn.innerText =="pause") {
+      //     result ="resume";
+      //     stopTimer()
+      //     togglePause("resume")
+      //   }else {(pauseBtn.innerText =="resume")
+      //    result ="pause"; 
+      //       setInterval()
+      //       togglePause("pause")
+      //     }
+      //     return result;
+      //   }
+      //   console.log(pauseBtn.innerText)
+      //   togglePause()
